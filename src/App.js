270a1f654f3de1d82e60a952e5fb0a4f37f3d070
Ori Korngut <ori.korngut@gmail.com>
2024-04-28 18:11:51 +0300

    Start working at QR

--- src/App.js
+++ src/App.js
@@ -16,22 +16,45 @@ function App() {
 }
 
 function Form() {
+    const [formData, setFormData] = useState({ Sname: '', Alliance: '' });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        generateBarcode(formData);
+    };
+
     return (
-        <form>
-            <label htmlFor="fname">First name of the Scouter:</label><br />
-            <input type="text" id="Sname" name="Sname" /><br />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="Sname">First name of the Scouter:</label><br />
+            <input type="text" id="Sname" name="Sname" value={formData.Sname} onChange={handleInputChange} /><br />
             <br />
-            <label htmlFor="lname">Alliance:</label><br />
-            <input type="text" id="Alliance" name="Alliance" />
+            <label htmlFor="Alliance">Alliance:</label><br />
+            <input type="text" id="Alliance" name="Alliance" value={formData.Alliance} onChange={handleInputChange} />
+            <br />
+            <br />
+            <button type="submit">Submit</button>
         </form>
     );
 }
 
 function Field() {
-    const renderCheckbox = (topPercent, leftPercent) => {
+    const [checkboxes, setCheckboxes] = useState(Array(9).fill(false));
+
+    const handleCheckboxChange = (index) => {
+        const newCheckboxes = [...checkboxes];
+        newCheckboxes[index] = !newCheckboxes[index];
+        setCheckboxes(newCheckboxes);
+    };
+
+    const renderCheckbox = (topPercent, leftPercent, index) => {
         return (
-            <div style={{ position: 'absolute', top: `${topPercent}%`, left: `${leftPercent}%` }}>
-                <input type="checkbox" />
+            <div key={index} style={{ position: 'absolute', top: `${topPercent}%`, left: `${leftPercent}%` }}>
+                <input type="checkbox" checked={checkboxes[index]} onChange={() => handleCheckboxChange(index)} />
             </div>
         );
     };
@@ -39,18 +62,29 @@ function Field() {
     return (
         <div style={{ position: 'relative' }}>
             <img src="https://www.chiefdelphi.com/uploads/default/original/3X/a/a/aa745548020a507cf4a07051dcd0faa446607840.png" alt="Field Image" className="center" />
-            {renderCheckbox(20, 56.6)}
-            {renderCheckbox(35, 56.6)}
-            {renderCheckbox(50, 56.6)}
-            {renderCheckbox(14.9, 49.5)}
-            {renderCheckbox(30.9, 49.5)}
-            {renderCheckbox(48.5, 49.5)}
-            {renderCheckbox(66.9, 49.5)}
-            {renderCheckbox(82.9, 49.5)}
-            {renderCheckbox(30.9, 49.5)}
+            {renderCheckbox(20, 56.6, 0)}
+            {renderCheckbox(35, 56.6, 1)}
+            {renderCheckbox(50, 56.6, 2)}
+            {renderCheckbox(14.9, 49.5, 3)}
+            {renderCheckbox(30.9, 49.5, 4)}
+            {renderCheckbox(48.5, 49.5, 5)}
+            {renderCheckbox(66.9, 49.5, 6)}
+            {renderCheckbox(82.9, 49.5, 7)}
+            {renderCheckbox(30.9, 49.5, 8)}
         </div>
     );
 }
 
+function generateBarcode(formData) {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const checkboxValues = Array.from(checkboxes).map(checkbox => checkbox.checked);
+
+    const data = { ...formData, checkboxes: checkboxValues };
+    const jsonStr = JSON.stringify(data);
+    const barcodeImageSrc = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(jsonStr)}&size=150x150`;
+
+    console.log(barcodeImageSrc);
+    // Display the barcode image or take further actions as needed
+}
 
 export default App;
