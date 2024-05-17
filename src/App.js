b2654ff1a5de4994fc0805ee074645f63e8d8485
AmitSu <sucher.amit@gmail.com>
2024-05-17 14:44:12 +0300

    added something that might ensure we can send data with internet with no need for qr code

--- src/App.js
+++ src/App.js
@@ -24,8 +24,28 @@ function App() {
         setFormData({ ...formData, checkboxes: newCheckboxes });
     };
 
+    const removeUnwantedCharacters = (value) => {
+        // Replace removes specific characters globally
+        return value.replace(/[\{\}\[\]]/g, '');
+    };
+
+    const sendDataToSheet = (value) => {
+        value = removeUnwantedCharacters(value);
+        fetch('https://script.google.com/macros/s/AKfycbwioY-LyzbjBEdEcK-FLFm3y0go1ohxY7g1MJRcq3yJ0CeafO1TM4FqHUbUZTQAKbo-/exec', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ value: value })
+        })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch(error => console.error('Error:', error));
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        sendDataToSheet(JSON.stringify(formData));
     };
 
     return (
@@ -34,28 +54,24 @@ function App() {
             <br/>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="Sname">Name of the Scouter:</label><br/>
-                <input type="text" id="Sname" name="Sname" value={formData.Sname} onChange={handleInputChange}/><br/>
+                <input type="text" id="Sname" name="Name" value={formData.Name} onChange={handleInputChange}/><br/>
                 <br/>
                 <label htmlFor="Team">Team number of the Scouter:</label><br/>
                 <input type="number" id="Team" name="Team" value={formData.Team} onChange={handleInputChange}/><br/>
                 <br/>
                 <label htmlFor="TeleNotes">Teleop Notes:</label><br/>
-                <input type="number" id="TeleNotes" name="TeleNotes" onChange={handleInputChange}/><br/>
+                <input type="number" id="TeleNotes" name="TeleNotes" value={formData.TeleNotes} onChange={handleInputChange}/><br/>
                 <br/>
                 <label htmlFor="Alliance">Alliance:</label><br/>
-                <input type="text" id="Alliance" name="Alliance" value={formData.Alliance}
-                       onChange={handleInputChange}/>
+                <input type="text" id="Alliance" name="Alliance" value={formData.Alliance} onChange={handleInputChange}/>
                 <br/>
+                <br/>
+                <button type="submit">Submit</button>
             </form>
-            <br/>
             <br/>
             <h3>Map for autonomous:</h3>
             <Field formData={formData} handleCheckboxChange={handleCheckboxChange}/>
             <br/>
-            <div className="center">
-                <button>Submit</button>
-            </div>
-
             <h3>If you're offline:</h3>
             <div style={{textAlign: 'center'}}>
                 <QRCode value={barcodeData} size={150}/>
@@ -64,16 +80,15 @@ function App() {
     );
 }
 
-function Field({formData, handleCheckboxChange}) {
+function Field({ formData, handleCheckboxChange }) {
     return (
-        <div style={{position: 'relative'}}>
+        <div style={{ position: 'relative' }}>
             <img
                 src="https://www.chiefdelphi.com/uploads/default/original/3X/a/a/aa745548020a507cf4a07051dcd0faa446607840.png"
                 alt="Field Image" className="center"/>
             {formData.checkboxes.map((checked, index) => (
-                <div key={index}
-                     style={{position: 'absolute', top: `${getFieldTop(index)}%`, left: `${getFieldLeft(index)}%`}}>
-                    <input type="checkbox" checked={checked} onChange={() => handleCheckboxChange(index)}/>
+                <div key={index} style={{ position: 'absolute', top: `${getFieldTop(index)}%`, left: `${getFieldLeft(index)}%` }}>
+                    <input type="checkbox" checked={checked} onChange={() => handleCheckboxChange(index)} />
                 </div>
             ))}
         </div>
@@ -119,7 +134,5 @@ function getFieldLeft(index) {
         default: return 0;
     }
 }
-
-
 
 export default App;
