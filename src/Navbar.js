7bcd1dfc5fae09b742010c5a0fe78704ebbd0908
Ori Korngut <ori.korngut@gmail.com>
2024-05-21 15:36:54 +0300

    With Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <a href="#home">בית</a>
+            <a href="#Login">Simbucks</a>
+            <a href="#Quals">מקצים</a>
+            <a href="#Simbucks">חשבון</a>
+        </div>
+    );
+}
+
+export default Navbar;
